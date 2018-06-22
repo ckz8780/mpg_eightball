0cc40b9832bd350f72f1534b16a444c293624e5d
Christopher Zielinski <ckz8780@gmail.com>
2018-06-22 16:36:28 -0400

    Handle populating output div and history table

--- static/js/base.js
+++ static/js/base.js
@@ -1,10 +1,16 @@
 $('#question-form').submit(function(e) {
 	e.preventDefault();
-	let submittedQuestion = $.trim(this.question.value);
-	this.reset();
+
+	// remove question marks and whitespace to prevent CORS request blocking
+	let submittedQuestion = $.trim(this.question.value).replace(/\?/g, '');
 	if(submittedQuestion.length > 1) {
+		this.reset();
 		$.get('https://8ball.delegator.com/magic/JSON/' + submittedQuestion).done(function(data) {
 			$('#answer').html(data.magic.answer);
+
+			let historyHtml = $('#response-history-tbody').html()
+			historyHtml += `<tr><td><strong>${data.magic['type']}</strong></td></tr>`
+			$('#response-history-tbody').html(historyHtml);
 		});
 	} else {
 		$('#missing-question').modal('show');
