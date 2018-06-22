0678e4fa4bb34e2ee3e465cb4d77e9a77e2741cc
Christopher Zielinski <ckz8780@gmail.com>
2018-06-22 18:56:51 -0400

    Added row limitation to history table

--- static/js/base.js
+++ static/js/base.js
@@ -2,14 +2,23 @@ $('#question-form').submit(function(e) {
 	e.preventDefault();
 
 	// remove question marks and whitespace to prevent CORS request blocking
-	let submittedQuestion = $.trim(this.question.value).replace(/\?/g, '');
+	// Other characters may still cause this
+	let submittedQuestion = $.trim(this.question.value.replace(/\?/g, ''));
 	if(submittedQuestion.length > 1) {
+		let uri = 'https://8ball.delegator.com/magic/JSON/' + encodeURIComponent(submittedQuestion);
 		this.reset();
-		$.get('https://8ball.delegator.com/magic/JSON/' + submittedQuestion).done(function(data) {
+		$.get(uri).done(function(data) {
 			$('#answer').html(data.magic.answer);
 
-			let historyHtml = $('#response-history-tbody').html()
-			historyHtml += `<tr><td><strong>${data.magic['type']}</strong></td></tr>`
+			var resultRowCount = $('#response-history-tbody tr').length;
+			var historyHtml = $('#response-history-tbody').html();
+			
+			if(resultRowCount >= 3) {
+				$('#response-history-tbody tr:eq(0)').remove();
+				historyHtml = $('#response-history-tbody').html();
+			}
+			
+			historyHtml += `<tr><td><strong>${data.magic['type']}</strong></td></tr>`;
 			$('#response-history-tbody').html(historyHtml);
 		});
 	} else {
