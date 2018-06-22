7ec46da2a8e15bace88e444c063b866e97a5235e
Christopher Zielinski <ckz8780@gmail.com>
2018-06-22 14:37:29 -0400

    Changed default runserver port via manage.py

--- manage.py
+++ manage.py
@@ -4,6 +4,16 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mpg_eightball.settings")
+
+    # Override default port for `runserver` command
+    try:
+        import django
+        django.setup()
+        from django.core.management.commands.runserver import Command as runserver
+        runserver.default_port = "21337"
+    except Exception as e:
+        print('\nWarning: Exception occurred while trying to override the default port for runserver. Defaulting to 8000. You can override it manually with \'python manage.py runserver <port>\'\n\nException: {}'.format(e))
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
