e423f7b3a32aef5de3f6d4177de2827b8a34741e
Christopher Zielinski <ckz8780@gmail.com>
2018-06-23 02:40:34 -0400

    2nd pass at local response history storage; Added multi-user support and clear-history button - OK

--- static/js/base.js
+++ static/js/base.js
@@ -1,12 +1,5 @@
-function restoreHistoryIfExists(user) {
-	var userHistoryObj = JSON.parse(localStorage.getItem(`userHistory_${user}`));
-	$('#response-history-tbody').html(userHistoryObj.historyHtml);
-}
-
-function getClass(resType) {
-	
-	// Returns a class name given a response type
-
+/** Returns a class name given a response type */
+function getCssClass(resType) {
 	switch(resType) {
 		case 'Contrary':
 			return 'text-danger';
@@ -16,53 +9,72 @@ function getClass(resType) {
 	return 'text-secondary';
 }
 
+/** Restore response history from localStorage if it exists */
+function restoreHistoryIfExists(user) {
+	var userHistoryObj = JSON.parse(localStorage.getItem(`userHistory_${user}`));
+	if(userHistoryObj) {
+		$('#clear-history-button').fadeToggle();
+		$('#response-history-tbody').html(userHistoryObj.historyHtml);
+	}
+}
+
+/** Store response history in localStorage */
 function storeHistory(user, historyHtml) {
-	console.log('User is: ', user);
-	console.log('History String:', historyHtml);
 	let userHistory = {
 		'username': user,
 		'historyHtml': historyHtml
 	};
-	console.log(userHistory);
 	localStorage.setItem(`userHistory_${user}`, JSON.stringify(userHistory));
+	$('#clear-history-button').fadeIn();
 }
 
+/** Handle yes/no question form submission */
 $('#question-form').submit(function(e) {
 	e.preventDefault();
 
-	// remove question marks and whitespace to prevent CORS request blocking
-	// Other characters may still cause this
 	let submittedQuestion = $.trim(this.question.value.replace(/\?/g, ''));
 	let currentUser = $.trim(this.currentUser.value);
+	
 	if(submittedQuestion.length > 1) {
 		let uri = 'https://8ball.delegator.com/magic/JSON/' + encodeURIComponent(submittedQuestion);
 		this.reset();
+	
 		$.get(uri).done(function(data) {
 			$('#answer').hide().html(`${data.magic.answer}.`).fadeIn();
 
-			var resultRowCount = $('#response-history-tbody tr').length;
+			var numRows = $('#response-history-tbody tr').length;
 			var historyHtml = $('#response-history-tbody').html();
 			
-			if(resultRowCount >= 3) {
+			if(numRows >= 3) {
 				$('#response-history-tbody tr:eq(0)').fadeOut().remove();
 				historyHtml = $('#response-history-tbody').html();
 			}
+
 			let resType = data.magic['type'];
-			historyHtml += `<tr><td><strong class="${getClass(resType)}">${resType}</strong></td></tr>`;
+			historyHtml += `<tr><td><strong class="${getCssClass(resType)}">${resType}</strong></td></tr>`;
+			
 			$('#response-history-tbody').html(historyHtml);
 			storeHistory(currentUser, historyHtml);
 		});
+
 	} else {
 		$('#invalid-question').modal('show');
 	}
 });
 
-
-// Smooth Nav Menu Transitions
+/** Smooth Nav Menu Transitions */
 $('.dropdown').on('show.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown(50);
 });
 
 $('.dropdown').on('hide.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp(50);
+});
+
+/** Find the current user's local response history and dump it */
+$('#clear-history-button').click(function() {
+	let currentUser = $('#currentUser').attr('value');
+	localStorage.removeItem(`userHistory_${currentUser}`);
+	$('#response-history-tbody').html('');
+	$(this).toggle();
 });
