63d8868f363b1786b9b13632c00930ea4735b84a
Christopher Zielinski <ckz8780@gmail.com>
2018-06-23 00:19:08 -0400

    more nav styling and tested user registration - OK

--- static/js/base.js
+++ static/js/base.js
@@ -38,3 +38,13 @@ $('#question-form').submit(function(e) {
 		$('#invalid-question').modal('show');
 	}
 });
+
+
+// Smooth Nav Menu Transitions
+$('.dropdown').on('show.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(50);
+});
+
+$('.dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(50);
+});
